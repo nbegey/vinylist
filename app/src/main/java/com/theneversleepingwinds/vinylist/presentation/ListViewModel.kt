f60590661c579b33cbb05bc1404b8b0363b7b543
Nicolas BEGEY <nicolas.begey@desjardins.com>
2018-03-07 21:41:32 -0500

    Adding librairies

---
+++ app/src/main/java/com/theneversleepingwinds/vinylist/presentation/ListViewModel.kt
@@ -0,0 +1,9 @@
+package com.theneversleepingwinds.vinylist.presentation
+
+import android.arch.lifecycle.ViewModel
+import com.theneversleepingwinds.vinylist.presentation.model.VinylPresentation
+
+/**
+ * @Author Nicolas BEGEY - 18-03-07.
+ */
+class ListViewModel(var vinyls: List<VinylPresentation>): ViewModel()
--- app/src/main/java/com/theneversleepingwinds/vinylist/presentation/fragment/ListFragment.java
+++ app/src/main/java/com/theneversleepingwinds/vinylist/presentation/fragment/ListFragment.java
@@ -1,5 +1,7 @@
 package com.theneversleepingwinds.vinylist.presentation.fragment;
 
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
@@ -7,6 +9,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import com.theneversleepingwinds.vinylist.R;
+import com.theneversleepingwinds.vinylist.presentation.ListViewModel;
 import com.theneversleepingwinds.vinylist.presentation.contract.ListContract;
 import com.theneversleepingwinds.vinylist.presentation.model.VinylPresentation;
 import com.theneversleepingwinds.vinylist.presentation.presenter.ListPresenter;
@@ -33,8 +36,12 @@ public class ListFragment extends BaseFragment implements ListContract.View{
     private List<VinylViewItem> mVinyls;
     private ListAdapter mAdapter;
 
+    private ListViewModel mListViewModel;
+
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mListViewModel = ViewModelProviders.of(this).get(ListViewModel.class);
 
         mPresenter = new ListPresenter(getContext());
         mPresenter.attachView(this);
